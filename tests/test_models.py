16235c4eb81f952b76185a2f4edb52a3cc1f12ae
oussamaok04 <oussama.okbani2013@gmail.com>
2025-10-30 15:26:37 -0400

    added test by price

--- tests/test_models.py
+++ tests/test_models.py
@@ -193,3 +193,15 @@ class TestProductModel(unittest.TestCase):
         self.assertEqual(found.count(), count)
         for product in found:
             self.assertEqual(product.category, category)
+
+    def test_find_product_by_price(self):
+        """It should search products by price"""
+        products = ProductFactory.create_batch(10)
+        for product in products:
+            product.create()
+        price = products[0].price
+        count = len([product for product in products if product.price == price])
+        found = Product.find_by_price(price)
+        self.assertEqual(found.count(), count)
+        for product in found:
+            self.assertEqual(product.price, price)
